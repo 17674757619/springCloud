bcbc8189ae58bc5e5cdf81355a548d570bedd247
17674757619 <1174578722@qq.com>
2019-12-19 17:02:13 +0800

    springcloud项目第一次提交

---
+++ serice-feign/src/main/java/com/example/demo/hystrix/HiServiceHystrix.java
@@ -0,0 +1,13 @@
+package com.example.demo.hystrix;
+
+import com.example.demo.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HiServiceHystrix implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
+
